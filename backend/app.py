49946cd392f8cd060cf505ee9be200817b3ccc76
tianjialin1010 <149170844+tianjialin1010@users.noreply.github.com>
2024-03-15 08:41:29 +0800

    Vue framework deployment

--- backend/app.py
+++ backend/app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def hello_world():
-    return 'Hello, World!'
+    return "<p>Hello, World!</p>"
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
